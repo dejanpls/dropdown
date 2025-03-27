ffc876e72c8338bb5ec0deeb8f52a2d952189b6f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-03-27 08:44:39 +0100

    docs: disable cache busting and  rename javascript filename to dropdown.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,14 @@
 // webpack.prod.js
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   output: {
-    filename: "main.[contenthash].js", // Cache busting
+    filename: 'dropdown.js', // disable cache busting
   },
   optimization: {
     minimize: true,
@@ -16,14 +16,14 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
